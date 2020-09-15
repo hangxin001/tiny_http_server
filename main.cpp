bc6e61165886f523fad8340ae6b391b19909813d
hangxin001 <a15059361492@gmial.com>
2020-09-15 21:19:52 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-///////////////////This is test file///////////////////
+///////////////////This is test file/////////////////
+///////////////////Test git and vs2019//////////////
 int main() {
 	std::cout << "hell" << std::endl;
 }
